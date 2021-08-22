1da0adcb3541c7802a3ed322dd04aef522eaf623
Ricardo Costa <ricardo.parallax@gmail.com>
2021-08-22 10:39:17 -0300

    Adiciona comentários aos métodos principais

--- RMC.DTO.CRM/RMC.DTO.CRM/Interface/IRepositoryCRM.cs
+++ RMC.DTO.CRM/RMC.DTO.CRM/Interface/IRepositoryCRM.cs
@@ -6,15 +6,62 @@ using System.Collections.Generic;
 
 namespace RMC.DTO.CRM.Interface
 {
+    /// <summary>
+    /// Interage com uma entidade do Microsoft Dynamics OnLine
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     interface IRepositoryCRM<T> where T : class
     {
+        /// <summary>
+        /// Cria um novo registro no Microsoft Dynamics
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         Guid Create(T entity);
+
+        /// <summary>
+        /// Atualiza um registro no Dynamics
+        /// </summary>
+        /// <param name="entity"></param>
         void Update(T entity);
+
+        /// <summary>
+        /// Recupera um Registro do dynamics acessando diretamente pelo PrimaryKey
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="nomeLogico"></param>
+        /// <returns></returns>
         T GetById(Guid id, string nomeLogico);
+
+        /// <summary>
+        /// Executa uma busca customizada no Dynamics, utilizada para permitir que o Repositório seja exposto em uma API sem necessadade que o cliente utiliza o CRMSDK
+        /// </summary>
+        /// <param name="nomeLogico"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         List<T> ListByFilter(string nomeLogico, QueryRequest filter);
+
+        /// <summary>
+        /// Executa uma busca no Microsoft Dynamics e recebe como parametro uma FilterExpression do SDK do dynamics
+        /// </summary>
+        /// <param name="nomeLogico"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         List<T> ListByFilter(string nomeLogico, FilterExpression filter);
 
+        /// <summary>
+        /// Exclui um registro de uma tabela do dynamics
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="nomeLogico"></param>
         void Delete(Guid id, string nomeLogico);
+
+        /// <summary>
+        /// Recurar o enumerador de um campo OptionSet do Dynamics
+        /// </summary>
+        /// <param name="entityName"></param>
+        /// <param name="attributeName"></param>
+        /// <returns></returns>
         List<ConjuntoOpcoesCRM> GetOptionSet(string entityName, string attributeName);
 
     }
